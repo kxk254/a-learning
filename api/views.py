91d4d392dcc8744562138300e910431a9e644b22
konno <konno.kenji@gmail.com>
2026-03-18 22:00:29 +0900

    2

--- api/views.py
+++ api/views.py
@@ -5,6 +5,7 @@ from api.models import Product, Order
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 # @api_view(['GET'])
 # def product_list(request):
@@ -34,6 +35,7 @@ class OrderListAPIView(generics.ListAPIView):
 class UserOrderListAPIView(generics.ListAPIView):
     queryset = Order.objects.prefetch_related('items__product')
     serializer_class = OrderSerializer
+    permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
         qs = super().get_queryset()
