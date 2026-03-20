e7a4c4fad0166f3a8da54737bd82b53a32ddcd3c
konno <konno.kenji@gmail.com>
2026-03-20 22:09:37 +0900

    2

--- api/views.py
+++ api/views.py
@@ -5,7 +5,7 @@ from api.models import Product, Order
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
 from rest_framework.views import APIView
 
 # @api_view(['GET'])
@@ -17,6 +17,12 @@ from rest_framework.views import APIView
 class ProductListCreateAPIView(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_permissions(self):
+        self.permission_classes = [AllowAny]
+        if self.request.method == 'POST':
+            self.permission_classes = [IsAdminUser]
+        return super().get_permissions()
 
 # class ProductCreateAPIView(generics.CreateAPIView):
 #     model = Product
