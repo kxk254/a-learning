cad9308a2cdf88b15956252d42856594c1412fd3
konno <konno.kenji@gmail.com>
2026-03-22 17:28:00 +0900

    2

--- api/filters.py
+++ api/filters.py
@@ -6,6 +6,6 @@ class ProductFilter(django_filters.FilterSet):
     class Meta:
         model = Product
         fields = {
-            "name": ["exact", "contains"],
+            "name": ["iexact", "icontains"],
             "price": ["exact", "lt", "gt", "range"],
         }
