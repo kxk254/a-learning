16d412f9e4a6f5b5af52acdb64187359b19f10bc
konno <konno.kenji@gmail.com>
2026-03-28 15:13:34 +0000

    2

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,19 +6,20 @@ import { User } from "./users/user.entity";
 
 @Module({
   imports: [
-	  ConfigModule.forRoot({isGlobal:true}),
-    TypeOrmModule.forRot({
-	    imports:[ConfigModule],
-	    inject:[ConfigSerivce],
-	    useFactory:(config:ConfigService)=>({
-      type: config.get<'postgres'>('DB_TYPE'),
-      host: config.get<string>('DB_HOST'),
-      port: config.get<number>('DB_PORT'),
-      username: config.get<string>('DB_USERNAME'),
-      password: config.get<string>('DB_PASSWORD'),
-      database: config.get<string>('DB_NAME''),
-      entities: [User],
-      synchronize: true,
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: config.get<"postgres">("DB_TYPE"),
+        host: config.get<string>("DB_HOST"),
+        port: config.get<number>("DB_PORT"),
+        username: config.get<string>("DB_USERNAME"),
+        password: config.get<string>("DB_PASSWORD"),
+        database: config.get<string>("DB_NAME"),
+        entities: [User],
+        synchronize: true,
+      }),
     }),
     UsersModule,
   ],
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { User } from "./user.entity";
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(User)
+    private usersRepository: Repository<User>,
+  ) {}
+
+  create(user: Partial<User>) {
+    const newUser = this.usersRepository.create(user);
+    return this.usersRepository.save(newUser);
+  }
+  findAll() {
+    return this.usersRepository.find();
+  }
+  findOne(id: number) {
+    return this.usersRepository.findOneBy({ id });
+  }
+  remove(id: number) {
+    return this.usersRepository.delete(id);
+  }
+}
