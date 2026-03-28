ed684aa80ce4f6af99b89983bd227253b5699f89
konno <konno.kenji@gmail.com>
2026-03-28 14:20:36 +0000

    2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from "./users/users.module";
+import { User } from "./users/user.entity";
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRot({
+      type: "postgres",
+      host: "192.168.11.71",
+      port: 5432,
+      username: "konno",
+      password: "Dev0126",
+      database: "test01",
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule,
+  ],
 })
 export class AppModule {}
