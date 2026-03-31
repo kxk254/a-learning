e4ed92063521edd37a1269cb4f4c4c351b61c2b7
konno <konno.kenji@gmail.com>
2026-03-31 22:00:12 +0900

    2

--- drizzle/migrations/0000_needy_calypso.sql
+++ drizzle/migrations/0000_needy_calypso.sql
@@ -2,13 +2,13 @@ CREATE TABLE "posts" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"title" varchar(200) NOT NULL,
 	"content" text,
-	"author_id" integer NOT NULL
+	"author_id" integer
 );
 --> statement-breakpoint
 CREATE TABLE "users" (
 	"id" serial PRIMARY KEY NOT NULL,
 	"name" varchar(50) NOT NULL,
-	"email" varchar(100) NOT NULL
+	"email" varchar(200) NOT NULL
 );
 --> statement-breakpoint
-ALTER TABLE "posts" ADD CONSTRAINT "posts_author_id_users_id_fk" FOREIGN KEY ("author_id") REFERENCES "public"."users"("id") ON DELETE CASCADE ON UPDATE no action;
+ALTER TABLE "posts" ADD CONSTRAINT "posts_author_id_users_id_fk" FOREIGN KEY ("author_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -4,6 +4,7 @@ import { UserService } from './user.service';
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
   @Post()
   async createUser(@Body() body: { name: string; email: string }) {
     return this.userService.createUser(body.name, body.email);
