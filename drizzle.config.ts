cfa7bcc27096e79fa3355e4b1f2e056b96964f74
konno <konno.kenji@gmail.com>
2026-04-02 23:18:53 +0900

    2

--- drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
-
-export default defineConfig({
-  dialect: 'postgresql',
-  schema: './src/db/schema.ts',
-  out: './drizzle/migration',
-  dbCredentials: { url: process.env.DATABASE_URL! },
-});
