4f5df21a07845794a8b86cce69e5646f5df84f0a
konno <konno.kenji@gmail.com>
2026-04-03 23:06:47 +0900

    2

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,23 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { db, usersTable } from '../db/db';
-
 export type User = any;
-
 @Injectable()
 export class UsersService {
   private readonly users = [
-    {
-      userId: 1,
-      username: 'john',
-      password: 'changeme',
-    },
-    {
-      userId: 2,
-      username: 'maria',
-      password: 'guess',
-    },
+    { userId: 1, username: 'john', password: 'changeme' },
+    { userId: 2, username: 'maria', password: 'guess' },
   ];
-
   async findOne(username: string): Promise<User | undefined> {
     return this.users.find((user) => user.username === username);
   }
