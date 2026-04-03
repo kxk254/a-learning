4d03ab578caa2346d1b7733b3b4388393ceafebd
konno <konno.kenji@gmail.com>
2026-04-03 04:52:08 +0000

    2

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,9 @@
+import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   dialect: 'postgresql',
   schema: './src/db/schema.ts',
   out: './migration',
-  deCredentials: { url: process.env.DATABASE_URL! },
+  dbCredentials: { url: process.env.DATABASE_URL! },
 });
--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,10 +1,9 @@
-import { pgTable, uuid, text, timestamp } from 'drizzle-orm/pg-core';
+import { pgTable, uuid, text, timestamp, varchar } from 'drizzle-orm/pg-core';
 
 export const users = pgTable('users', {
   id: uuid('id').defaultRandom().primaryKey(),
+  name: varchar('name').notNull(),
   email: text('email').notNull().unique(),
   password: text('password').notNull(),
   role: text('role').notNull().default('user'),
-  createdAt: timestamp('created_at').defaultNow().notNull(),
-  updatedAt: timestamp('updaated_at').defaultNow().notNull(),
 });
