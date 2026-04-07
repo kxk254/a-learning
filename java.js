e75e36f38d6677e6733c8fb0f8e5c324a7078a5e
konno <konno.kenji@gmail.com>
2026-04-07 20:32:29 +0900

    javascript

---
+++ java.js
@@ -0,0 +1,27 @@
+function resolveAfter2Seconds(x) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(x);
+    }, 2000);
+  });
+}
+
+// async function expression assigned to a variable
+const add = async function (x) {
+  const a = await resolveAfter2Seconds(50);
+  const b = await resolveAfter2Seconds(50);
+  return x + a + b;
+};
+
+add(10).then((v) => {
+  console.log(v); // prints 60 after 4 seconds.
+});
+
+// async function expression used as an IIFE
+(async function (x) {
+  const p1 = resolveAfter2Seconds(20);
+  const p2 = resolveAfter2Seconds(30);
+  return x + (await p1) + (await p2);
+})(10).then((v) => {
+  console.log(v); // prints 60 after 2 seconds.
+});
