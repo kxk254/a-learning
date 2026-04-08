85540aafdecdc5959be4bf49c30b561361f26322
konno <konno.kenji@gmail.com>
2026-04-08 04:48:54 +0000

    java

--- java.js
+++ java.js
@@ -1,20 +1,20 @@
-function doSomething() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      console.log('Did something');
-      resolve('https://example.com');
-    }, 200);
-  });
+async function fetchProducts() {
+  try {
+    // after this line, our function will wait for the `fetch()` call to be settled
+    // the `fetch()` call will either return a Response or throw an error
+    const response = await fetch(
+      'https://mdn.github.io/ldearning-area/javascript/apis/fetching-data/can-store/products.json',
+    );
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    // after this line, our function will wait for the `response.json()` call to be settled
+    // the `response.json()` call will either return the parsed JSON object or throw an error
+    const data = await response.json();
+    console.log(data[0].name);
+  } catch (error) {
+    console.error(`Could not get products: ${error}`);
+  }
 }
 
-doSomething()
-  .then(function (result) {
-    return doSomething(result);
-  })
-  .then(function (newResult) {
-    console.log(newResult);
-    return doSomethingElse(newResult);
-  })
-  .then(function (finalresult) {
-    console.log(`got the final result:${finalresult}`);
-  });
+fetchProducts();
