3dc849a87a9d5c5f8dba0fca018a7eacc094dae1
konno <konno.kenji@gmail.com>
2026-04-09 05:41:08 +0000

    java

--- java.js
+++ java.js
@@ -1,5 +1,5 @@
 //savacript
-const res = await fetch('http://localhost:3000/auth/login', {
+const tok = await fetch('http://localhost:3000/auth/login', {
   method: 'POST',
   headers: { 'Content-Type': 'application/json' },
   body: JSON.stringify({
@@ -8,5 +8,16 @@ const res = await fetch('http://localhost:3000/auth/login', {
   }),
 });
 
-const data = await res.json();
-console.log(data);
+const token = await tok.json();
+console.log(token.access_token);
+
+const res = await fetch('http://localhost:3000/cats/1', {
+  method: 'GET',
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${token}`,
+  },
+});
+
+const response = await res.json();
+console.log(response);
