40aa567505239f83d16992f46e8bfb5cc9cd13aa
konno <konno.kenji@gmail.com>
2026-04-11 00:56:24 +0900

    form

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,40 @@
 "use client";
 import { useState } from "react";
-import "./styles.css";
 
-function Square({ value, onSquareClick }) {
-  return (
-    <button className="square" onClick={onSquareClick}>
-      {value}
-    </button>
-  );
-}
-export default function Board() {
-  const [values, setValues] = useState(Array(9).fill(null));
-  function handleClick(i) {
-    const nextValues = values.slice();
-    nextValues[i] = "X";
-    setValues(nextValues);
+export default function MyObject() {
+  const [form, setForm] = useState({
+    firstName: "Kenji",
+    lastName: "Konno",
+    email: "kkonno@soliton-cm.com",
+  });
+
+  function fnHandler(e) {
+    setForm({ ...form, firstName: e.target.value });
   }
+  function lnHandler(e) {
+    setForm({ ...form, lastName: e.target.value });
+  }
+  function emHandler(e) {
+    setForm({ ...form, email: e.target.value });
+  }
+
   return (
-    <div>
-      <div className="board-row">
-        <Square value={values[0]} onSquareClick={() => handleClick(0)} />
-        <Square value={values[1]} onSquareClick={() => handleClick(1)} />
-        <Square value={values[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
-        <Square value={values[3]} onSquareClick={() => handleClick(3)} />
-        <Square value={values[4]} onSquareClick={() => handleClick(4)} />
-        <Square value={values[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
-        <Square value={values[6]} onSquareClick={() => handleClick(6)} />
-        <Square value={values[7]} onSquareClick={() => handleClick(7)} />
-        <Square value={values[8]} onSquareClick={() => handleClick(8)} />
-      </div>
-    </div>
+    <>
+      <p>
+        FirstName:
+        <input value={form.firstName} onChange={fnHandler} />
+      </p>
+      <p>
+        LastName:
+        <input value={form.lastName} onChange={lnHandler} />
+      </p>
+      <p>
+        Email:
+        <input value={form.email} onChange={emHandler} />
+      </p>
+      <p>
+        {form.firstName} | {form.lastName} - ({form.email})
+      </p>
+    </>
   );
 }
