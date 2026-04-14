4abf9754300d6f80c3b3984a0a2fa6a4c537e71a
konno <konno.kenji@gmail.com>
2026-04-14 22:30:34 +0900

    callback

--- app/page.tsx
+++ app/page.tsx
@@ -4,34 +4,26 @@ import { useState } from "react";
 import { useReducer } from "react";
 import { useRef } from "react";
 
-export default function StopWatch() {
-  const [seconds, setSeconds] = useState(0);
-  const intervalRef = useRef(null);
+import ProductPage from "./ProductPage.js";
 
-  function start() {
-    if (intervalRef.current) return;
-    intervalRef.current = setInterval(() => {
-      setSeconds((p) => p + 1);
-    }, 10);
-  }
-
-  function stop() {
-    clearInterval(intervalRef.current);
-    intervalRef.current = null;
-  }
-
-  function reset() {
-    setSeconds(0);
-    stop();
-  }
-
+export default function App() {
+  const [isDark, setIsDark] = useState(false);
   return (
     <>
-      <h1>{seconds}</h1>
-      <button onClick={start}>Start</button>
-      <button onClick={stop}>Stop</button>
-      <button onClick={reset}>Reset</button>
-      <h1>{intervalRef.current ? intervalRef.current : "null"}</h1>
+      <label>
+        <input
+          type="checkbox"
+          checked={isDark}
+          onChange={(e) => setIsDark(e.target.checked)}
+        />
+        Dark mode
+      </label>
+      <hr />
+      <ProductPage
+        referrerId="wizard_of_oz"
+        productId={123}
+        theme={isDark ? "dark" : "light"}
+      />
     </>
   );
 }
