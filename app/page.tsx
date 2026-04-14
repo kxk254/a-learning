25efd62b9adf31120380aa69157b9ff0d77b1f64
konno <konno.kenji@gmail.com>
2026-04-14 23:23:28 +0900

    context

--- app/page.tsx
+++ app/page.tsx
@@ -2,28 +2,107 @@
 import "./styles.css";
 import { useState } from "react";
 import { useReducer } from "react";
-import { useRef } from "react";
+import { useRef, createContext, useContext } from "react";
 
-import ProductPage from "./ProductPage.js";
+const ThemeContext = createContext(null);
+const CurrentUserContext = createContext(null);
 
-export default function App() {
-  const [isDark, setIsDark] = useState(false);
+export default function MyApp() {
+  const [theme, setTheme] = useState("light");
+  const [currentUser, setCurrentUser] = useState(null);
+  return (
+    <ThemeContext value={theme}>
+      <CurrentUserContext
+        value={{
+          currentUser,
+          setCurrentUser,
+        }}
+      >
+        <WelcomePanel />
+        <label>
+          <input
+            type="checkbox"
+            checked={theme === "dark"}
+            onChange={(e) => {
+              setTheme(e.target.checked ? "dark" : "light");
+            }}
+          />
+          Use dark mode
+        </label>
+      </CurrentUserContext>
+    </ThemeContext>
+  );
+}
+
+function WelcomePanel({ children }) {
+  const { currentUser } = useContext(CurrentUserContext);
+  return (
+    <Panel title="Welcome">
+      {currentUser !== null ? <Greeting /> : <LoginForm />}
+    </Panel>
+  );
+}
+
+function Greeting() {
+  const { currentUser } = useContext(CurrentUserContext);
+  return <p>You logged in as {currentUser.name}.</p>;
+}
+
+function LoginForm() {
+  const { setCurrentUser } = useContext(CurrentUserContext);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const canLogin = firstName.trim() !== "" && lastName.trim() !== "";
   return (
     <>
       <label>
+        First name{": "}
         <input
-          type="checkbox"
-          checked={isDark}
-          onChange={(e) => setIsDark(e.target.checked)}
+          required
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
         />
-        Dark mode
       </label>
-      <hr />
-      <ProductPage
-        referrerId="wizard_of_oz"
-        productId={123}
-        theme={isDark ? "dark" : "light"}
-      />
+      <label>
+        Last name{": "}
+        <input
+          required
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </label>
+      <Button
+        disabled={!canLogin}
+        onClick={() => {
+          setCurrentUser({
+            name: firstName + " " + lastName,
+          });
+        }}
+      >
+        Log in
+      </Button>
+      {!canLogin && <i>Fill in both fields.</i>}
     </>
+  );
+}
+
+function Panel({ title, children }) {
+  const theme = useContext(ThemeContext);
+  const className = "panel-" + theme;
+  return (
+    <section className={className}>
+      <h1>{title}</h1>
+      {children}
+    </section>
+  );
+}
+
+function Button({ children, disabled, onClick }) {
+  const theme = useContext(ThemeContext);
+  const className = "button-" + theme;
+  return (
+    <button className={className} disabled={disabled} onClick={onClick}>
+      {children}
+    </button>
   );
 }
