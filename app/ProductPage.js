ed861a5bfa2b99856fe072b5a68905fb82058541
konno <konno.kenji@gmail.com>
2026-04-15 23:50:38 +0900

    form

--- app/ProductPage.js
+++ app/ProductPage.js
@@ -3,12 +3,12 @@ import { useCallback } from "react";
 import ShippingForm from "./ShippingForm.js";
 
 export default function ProductPage({ referredId, productId, theme }) {
-  const handleSubmit = useCallback(
-    (orderDetails) => {
-      console.log("print", referredId, productId, orderDetails);
-    },
-    [referredId, productId],
-  );
+  const handleSubmit = (orderDetails) => {
+    console.log("print", referredId, productId, orderDetails);
+  };
+
+  console.log("function reference", handleSubmit);
+
   return (
     <div className={theme}>
       <ShippingForm onSubmit={handleSubmit} />
