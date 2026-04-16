44822717cdf538b6f9a77b7d1f981c32b4f3c28e
konno <konno.kenji@gmail.com>
2026-04-16 07:11:47 +0000

    form

--- app/ProductPage.js
+++ app/ProductPage.js
@@ -1,11 +1,17 @@
 "use client";
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import ShippingForm from "./ShippingForm.js";
 
 export default function ProductPage({ referredId, productId, theme }) {
-  const handleSubmit = (orderDetails) => {
-    console.log("print", referredId, productId, orderDetails);
-  };
+  const [lastorder, setLastorder] = useState(null);
+  const handleSubmit = useCallback(
+    (orderDetails) => {
+      setLastorder(orderDetails);
+      console.log("print", referredId, productId, orderDetails);
+      post("/product/" + productId + "/buy", { referredId, orderDetails });
+    },
+    [referredId, productId],
+  );
 
   console.log("function reference", handleSubmit);
 
@@ -14,4 +20,9 @@ export default function ProductPage({ referredId, productId, theme }) {
       <ShippingForm onSubmit={handleSubmit} />
     </div>
   );
+}
+
+function post(url, data) {
+  console.log(url);
+  console.log(data);
 }
