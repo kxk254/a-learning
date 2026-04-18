fd7cb3dbdf73a4495dda0738643aa9ea690984c1
konno <konno.kenji@gmail.com>
2026-04-18 19:25:09 +0900

    click

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
+console.log("reading javascript");
 const btn = document.getElementById("btn");
 const title = document.getElementById("title");
 const info = document.getElementById("info");
+let isClicked = false;
 
-btn.addEventListner("click", () => {
-  title.textContent = "You clicked the button!";
-  info.textContent = "JS is working!";
-  document.body.style.backgroundColor = "#f0f8ff";
+btn.addEventListener("click", () => {
+  if (!isClicked) {
+    title.textContent = "You clicked the button!";
+    info.textContent = "JS is working!";
+    document.body.style.backgroundColor = "#f0f8ff";
+  } else {
+    title.textContent = "Hello, I am learning JS";
+    info.textContent = "Nothing happened yet";
+    document.body.style.backgroundColor = "#ffffff";
+  }
+  isClicked = !isClicked;
 });
