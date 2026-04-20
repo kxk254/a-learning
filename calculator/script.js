6bc70101cb8f2ff28b02fa559448258dc68ffa1a
konno <konno.kenji@gmail.com>
2026-04-20 03:59:58 +0000

    change

--- calculator/script.js
+++ calculator/script.js
@@ -1,7 +1,10 @@
 const valA = document.getElementById("valueA");
 const valB = document.getElementById("valueB");
 const result = document.querySelector("#result");
-const btn = document.querySelector("#btn");
+const plus = document.querySelector("#plus");
+const minus = document.querySelector("#minus");
+const multiple = document.querySelector("#multiple");
+const divide = document.querySelector("#divide");
 
 btn.addEventListener("click", () => {
   console.log("A", valA.value);
