115bde00a9d0da5262ecf5ee73fe118535def5fe
konno <konno.kenji@gmail.com>
2026-04-29 23:46:43 +0900

    formadvance

--- form/script.js
+++ form/script.js
@@ -11,150 +11,172 @@ const totalField = document.querySelector("#totalField");
 // prevent invalid states prevent minus
 // persist + restore cleanly
 /*
-A. Event 1. (form - submit) 2.(data field - del / update) 3. (buttons - reset / load) 
-B. State 1 (Input Data / validate Input) 2. (sumTotal) 3,(delegate - delete / edit) 4. (crud) 
-C. 1. (commit) 2. (history) 3. (currentIndex)
-D. Side effects 
-1. (storage - save/load/reset) 2. (DOM partual updates)
-E. UI 1. (input field) 2. (data field - all by id) 3. (sum total) 
+ //* 0. app state
+ //* A. Events 
+ //* - submit
+ //* - edit 
+ //* - delete 
+ //* - buttons 
+ //* B. State Logic
+ //* - validate Input 
+ //* - CRUD 
+ //* - sum total 
+ //* C. Commit Layer
+ //* - setState 
+ //* - history 
+ //* - current Index
+ //* D. Render UI 
+ //* - render rows 
+ //* - render total 
+ //* - render input 
+ //* E. Side Effects
+ //* - DOM update 
+ //* - local Storage
  */
 // object
-let rows = [];
+//* 0. app state
+function createApp() {
+  let state = { rows: [] };
+  function setState(newState) {
+    state = newState;
+    renderAll(state);
+  }
+  return { getState: () => state, setState };
+}
+const app = createApp();
 
-// Event
+//* A. Events
+//* - submit
 myForm.addEventListener("submit", (e) => {
   e.preventDefault();
-  inputData();
+  const state = app.getState();
+  crud.createRow(state);
+  saveStorage(state);
 });
-// update input
-dataField.addEventListener("change", (e) => {
-  editableInput(e);
-});
-// delete delgator
-dataField.addEventListener("click", (e) => {
-  delegateDeleteButton(e);
-});
-resetBtn.addEventListener("click", () => {
-  resetStorage();
-});
-loadBtn.addEventListener("click", () => {
-  loadStorage();
+//* - edit
+inputField.addEventListener("change", (e) => {
+  const state = app.getState();
+  const rowEl = e.target.closest(".row");
+  if (!rowEl) return;
+  const id = Number(rowEl.dataset.id);
+  const name = e.target.name;
+  const value = e.target.value;
+  crud.updateRow(state, id, name, value);
+  saveStorage(state);
 });
 
-// Data State
-// 1. input data
-function inputData() {
-  try {
-    let price = validateInput(inputField.querySelector("[name='price']").value);
-    let qty = validateInput(inputField.querySelector("[name='qty']").value);
-    let data = crud.create(price, qty);
-    rows = [...rows, data];
-    saveStorage();
-    renderDataField();
-    renderInputField();
-    renderTotalField();
-  } catch (err) {
-    alert(err.message);
-  }
-}
-// 2. validate input
+//* - delete
+inputField.addEventListener("click", (e) => {
+  const state = app.getState();
+  if (!e.target.classList.contains("delete-btn")) return;
+  let rowEl = e.target.closest(".row");
+  const id = Number(rowEl.dataset.id);
+  const newState = crud.deleteRow(state, id);
+  saveStorage(newState);
+  renderAll(newState);
+});
+//* - buttons
+resetBtn.addEventListener("click", () => {
+  const state = app.getState();
+  resetStorage(state);
+  renderAll(state);
+});
+loadBtn.addEventListener("click", () => {
+  const state = app.getState();
+  loadStorage(state);
+  renderAll(state);
+});
+//* B. State Logic
+//* - validate Input
 function validateInput(input) {
   if (String(input).trim() === "") {
-    throw new Error("Empty value is not allowed");
+    throw new Error("Do not input Empty value");
   }
   const num = Number(input);
   if (!Number.isFinite(num)) {
     throw new Error("Enter a valid input");
+  } else if (num < 0) {
+    throw new Error("Negative is not allowed");
   }
   return num;
 }
-// 3. sum total
-function sumTotal() {
-  return rows.reduce(
+//* - CRUD
+const crud = {
+  createRow(state) {
+    try {
+      const p = validateInput(inputField.querySelector("[name='price']").value);
+      const q = validateInput(inputField.querySelector("[name='qty']").value);
+      const row = { id: Date.now(), price: p, qty: q };
+      console.log("create row", row);
+      const newState = { ...state, rows: [...state.rows, row] };
+      console.log("create new state", newState);
+      return newState;
+    } catch (err) {
+      alert(err.message);
+    }
+  },
+  updateRow(state, id, name, value) {
+    return {
+      ...state,
+      rows: rows.map((row) =>
+        row.id === Number(id) ? { ...row, [name]: value } : row,
+      ),
+    };
+  },
+  deleteRow(state, id) {
+    return {
+      ...state,
+      rows: rows.map((row) => row.id !== Number(id)),
+    };
+  },
+};
+//* - sum total
+function sumTotal(state) {
+  return state.rows.reduce(
     (acc, row) => {
       acc.totalPrice += Number(row.price);
       acc.totalQty += Number(row.qty);
-      return acc;
     },
     { totalPrice: 0, totalQty: 0 },
   );
 }
-// 4. delegate delete button
-function delegateDeleteButton(e) {
-  if (e.target.classList.contains("delete-btn")) {
-    const rowEl = e.target.closest(".row");
-    if (!rowEl) return;
-    const id = Number(rowEl.dataset.id);
-    crud.delData(id);
-    saveStorage();
-    renderDataField();
-    renderTotalField();
-  }
+//* C. Commit Layer
+//* - setState
+//* - history
+//* - current Index
+//* D. Render UI
+//* - render rows
+function renderRows(state) {
+  dataFieldDOM(state);
 }
-// 5. editable input
-function editableInput(e) {
-  const rowEl = e.target.closest(".row");
-  if (!rowEl) return;
-  const id = rowEl.dataset.id;
-  const name = e.target.name;
-  const value = validateInput(e.target.value.trim());
-  console.log("editing id:", id, "name", name, "value", value);
-  crud.update(id, name, value);
-  saveStorage();
-  renderDataField();
-  renderTotalField();
+//* - render total
+function renderTotal(state) {
+  totalFieldDOM(state);
 }
-// 6. data operator
-const crud = {
-  create(p, q) {
-    const temp = { id: Date.now(), price: p, qty: q };
-    const data = temp ? temp : [];
-    return data;
-  },
-  read() {},
-  update(id, name, value) {
-    rows = rows.map((row) =>
-      row.id === Number(id) ? { ...row, [name]: Number(value) } : row,
-    );
-  },
-
-  delData(id) {
-    rows = rows.filter((row) => row.id !== id);
-    return rows;
-  },
-};
-// LOCALSTORAGE
-// 1. save storage
-function saveStorage() {
-  console.log("save store", rows);
-  localStorage.setItem("rows", JSON.stringify(rows));
+//* - render input
+function renderInput() {
+  inputFieldDOM();
 }
-// 2. load storage
-function loadStorage() {
-  let temp = localStorage.getItem("rows");
-  rows = temp ? JSON.parse(temp) : [];
-  console.log("load ", rows);
-  renderDataField();
-  renderTotalField();
-  return rows;
+function renderAll(state) {
+  dataFieldDOM(state);
+  totalFieldDOM(state);
+  inputFieldDOM();
 }
-// 3. reset storage
-function resetStorage() {
-  localStorage.removeItem("rows");
-  rows = [];
-  renderDataField();
-  renderTotalField();
-  return rows;
+//* E. Side Effects
+//* - DOM update
+function inputFieldDOM() {
+  inputField.innerHTML = `
+<input type="text" name="price" value=""/>
+<input type="text" name="qty" value=""/>
+		`;
 }
 
-// UI
-// 1. daia field
-function renderDataField() {
+function dataFieldDOM(state) {
   dataField.innerHTML = "";
-  rows.forEach((row) => {
+  state.rows.forEach((row) => {
     const div = document.createElement("div");
     div.className = "row";
-    div.dataset.id = row.id;
+    div.dataset.id = Number(row.id);
     div.innerHTML = `
 	<input type="text" name="price" value="${row.price}"/>
 	<input type="text" name="qty" value="${row.qty}"/>
@@ -163,18 +185,25 @@ function renderDataField() {
     dataField.appendChild(div);
   });
 }
-// 2. input field
-function renderInputField() {
-  inputField.innerHTML = `
-<input type="text" name="price" value=""/>
-<input type="text" name="qty" value=""/>
-`;
-}
-// 3. total field
-function renderTotalField() {
-  let sum = sumTotal();
-  totalField.innerHTML = `
-<p>Total Price :<span>${sum.totalPrice}</span> | Total Qty: <span>${sum.totalQty}</span></p>
+
+function totalFieldDOM(state) {
+  const sum = sumTotal(state);
+  toalField.innerHTML = `
+<p>Total Price: ${sum.totalPrice} | Total Qty: ${sum.totalQty}</p>
 	`;
 }
-renderInputField();
+//* - local Storage
+function saveStorage(state) {
+  localStorage.setItem("rows", JSON.stringify(state));
+}
+function loadStorage(state) {
+  const temp = localStorage.getItem("rows");
+  const newState = temp ? JSON.parse(temp) : { rows: [] };
+  return state.setState(newState);
+}
+function resetStorage(state) {
+  localStorage.removeItem("rows");
+  const newState = { rows: [] };
+  return state.setState(newState);
+}
+renderInput();
