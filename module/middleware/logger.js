3bb1ded49c0cc2d09af54f2a4bedceb145f84e70
konno <konno.kenji@gmail.com>
2026-05-09 19:42:28 +0900

    form

--- module/middleware/logger.js
+++ module/middleware/logger.js
@@ -7,12 +7,3 @@ export const logger =
     console.log("NEW STATE", getState());
     return result;
   };
-export const logger =
-  ({ getState }) =>
-  (next) =>
-  (action) => {
-    console.log("ACTION", action);
-    const result = next(action);
-    console.log("NEW STATE", getState());
-    return result;
-  };
