e45e47d109a4725a7cfbf228f7090a30f87858a0
konno <konno.kenji@gmail.com>
2026-05-10 19:05:14 +0900

    module

--- module/store/loadDataThunk.js
+++ module/store/loadDataThunk.js
@@ -1,1 +1,13 @@
 import { loadMockData } from "./index.js";
+
+export function loadDataThunk() {
+  return async (dispatch, getState) => {
+    dispatch({ type: "loadStart" });
+    try {
+      const newPresent = await loadMockData();
+      dispatch({ type: "loadSuccess", payload: newPresent });
+    } catch (err) {
+      dispatch({ type: "loadError", payload: err.message });
+    }
+  };
+}
