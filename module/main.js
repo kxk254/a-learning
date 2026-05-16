4c74cef423f625e22769c9aa5fc197cd60d4bf6a
konno <konno.kenji@gmail.com>
2026-05-16 16:37:24 +0900

    form

--- module/main.js
+++ module/main.js
@@ -2,20 +2,3 @@ import { createApp, initialState, reducer } from "./store/index.js";
 import { render } from "./render/index.js";
 import { setupHandlers } from "./handlers/handlers.js";
 import { logger, persist, thunk } from "./middleware/index.js";
-
-const app = createApp(initialState, reducer, [logger, persist, thunk]);
-
-let prevState;
-app.subscribe(() => {
-  const state = app.getState();
-  console.log("prevState -->", prevState);
-  if (prevState !== state) {
-    console.log("entities === state");
-    render.renderAll(state);
-  }
-  prevState = state;
-});
-
-render.initialUI(app.getState());
-
-setupHandlers(app);
--- module/middleware/logger.js
+++
@@ -1,9 +0,0 @@
-export const logger =
-  ({ getState }) =>
-  (next) =>
-  (action) => {
-    console.log("ACTION", action, next);
-    const result = next(action);
-    console.log("NEW STATE", getState());
-    return result;
-  };
