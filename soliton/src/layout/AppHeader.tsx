26297ff3c04734f272d2fd4a80385a942bb56a26
konno <konno.kenji@gmail.com>
2026-05-31 23:49:26 +0900

    soliton

--- soliton/src/layout/AppHeader.tsx
+++ soliton/src/layout/AppHeader.tsx
@@ -5,6 +5,8 @@ import { useTheme } from "../context/ThemeContext";
 import { BarsStaggered } from "@/src/icons/index";
 import styles from "./Header.module.css";
 import { ThemeToggleBtn } from "@/src/components/header/ThemeToggleBtn";
+import Image from "next/image";
+import Link from "next/link";
 
 export const AppHeader = () => {
   const { theme, toggleTheme } = useTheme();
@@ -12,6 +14,25 @@ export const AppHeader = () => {
   return (
     <header className={styles.header}>
       <BarsStaggered />
+      <Link href="/" className={styles.logo}>
+        {theme === "light" ? (
+          <Image
+            className={styles.logoLight}
+            src="./images/logo/text-logo.svg"
+            alt="Logo"
+            width={154}
+            height={32}
+          />
+        ) : (
+          <Image
+            className={styles.logoDark}
+            src="./images/logo/text-logo-dark.svg"
+            alt="logo"
+            width={154}
+            height={32}
+          />
+        )}
+      </Link>
       <div className={styles.headerRight}>
         <ThemeToggleBtn />
       </div>
