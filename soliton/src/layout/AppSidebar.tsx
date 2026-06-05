1c6595073854204844de03c8dfa16714d1ee2906
konno <konno.kenji@gmail.com>
2026-06-05 07:01:47 +0000

    soliton

--- soliton/src/layout/AppSidebar.tsx
+++ soliton/src/layout/AppSidebar.tsx
@@ -1,8 +1,17 @@
 "use client";
 import React, { useEffect, useRef, useState, useCallback } from "react";
-import { BoxCubeIcon, CalendarIcon, UserCircleIcon } from "@/src/icons/index";
+import { usePathname } from "next/navigation";
+import Image from "next/image";
+import Link from "next/link";
 import styles from "./Sidebar.module.css";
 import { useSidebar } from "@/src/context/SidebarContext";
+import {
+  BoxCubeIcon,
+  CalendarIcon,
+  UserCircleIcon,
+  PieChartIcon,
+  PlugInIcon,
+} from "@/src/icons/index";
 
 type NavItem = {
   name: string;
@@ -33,21 +42,119 @@ const navItems: NavItem[] = [
   },
 ];
 
+const othersItems: NavItem[] = [
+  {
+    icon: <PieChartIcon />,
+    name: "Charts",
+    subItems: [
+      { name: "Line Chart", path: "/line-chart", pro: false },
+      { name: "Bar Chart", path: "/bar-chart", pro: false },
+    ],
+  },
+  {
+    icon: <BoxCubeIcon />,
+    name: "UI Elements",
+    subItems: [
+      { name: "Alerts", path: "/alerts", pro: false },
+      { name: "Avatar", path: "/avatars", pro: false },
+      { name: "Badge", path: "/badge", pro: false },
+      { name: "Buttons", path: "/buttons", pro: false },
+      { name: "Images", path: "/images", pro: false },
+      { name: "Videos", path: "/videos", pro: false },
+    ],
+  },
+  {
+    icon: <PlugInIcon />,
+    name: "Authentication",
+    subItems: [
+      { name: "Sign In", path: "/signin", pro: false },
+      { name: "Sign Up", path: "/signup", pro: false },
+    ],
+  },
+];
+
 export const AppSidebar = () => {
   const { isExpanded, isMobileOpen, isHovered, setIsHovered } = useSidebar();
+  const pathname = usePathname();
+
   const renderMenuItems = (
     navItems: NavItem[],
     menuType: "main" | "others",
   ) => (
-    <ul>
+    <ul className={styles.ulStyle}>
       {navItems.map((nav, index) => (
         <li key={nav.name}>
-          <span>{nav.icon}</span>
-          <span>{nav.name}</span>
+          {nav.subItems ? (
+            <button>
+              <span>{nav.icon}</span>
+              <span>{nav.name}</span>
+            </button>
+          ) : (
+            nav.path && (
+              <Link href={nav.path}>
+                <span>{nav.icon}</span>
+              </Link>
+            )
+          )}
         </li>
       ))}
     </ul>
   );
+  const [openSubmenu, setOpenSubmenu] = useState<{
+    type: "main" | "others";
+    index: number;
+  } | null>(null);
+  const [subMenuHeight, setSubMenuHeight] = useState<Record<string, number>>(
+    {},
+  );
+  const subMenuRefs = useRef<Record<string, HTMLDivElement | null>>({});
+  const isActive = useCallback((path: string) => path === pathname, [pathname]);
+
+  useEffect(() => {
+    let subMenuMatched = false;
+    ["main", "others"].forEach((menuType) => {
+      const items = menuType === "main" ? navItems : othersItems;
+      items.forEach((nav, index) => {
+        if (nav.subItems) {
+          nav.subItems.forEach((subItem) => {
+            if (isActive(subItem.path)) {
+              setOpenSubmenu({ type: menuType as "main" | "others", index });
+              subMenuMatched = true;
+            }
+          });
+        }
+      });
+    });
+
+    if (!subMenuMatched) {
+      setOpenSubmenu(null);
+    }
+  }, [pathname, isActive]);
+
+  useEffect(() => {
+    if (openSubmenu !== null) {
+      const key = `${openSubmenu.type}-${openSubmenu.index}`;
+      if (subMenuRefs.current[key]) {
+        setSubMenuHeight((prevHeights) => ({
+          ...prevHeights,
+          [key]: subMenuRefs.current[key]?.scrollHeight || 0,
+        }));
+      }
+    }
+  }, [openSubmenu]);
+
+  const handleSubmenuToggle = (index: number, menuType: "main" | "others") => {
+    setOpenSubmenu((prevOpenSubmenu) => {
+      if (
+        prevOpenSubmenu &&
+        prevOpenSubmenu.type === menuType &&
+        prevOpenSubmenu.index === index
+      ) {
+        return null;
+      }
+      return { type: menuType, index };
+    });
+  };
 
   return (
     <>
@@ -65,7 +172,26 @@ export const AppSidebar = () => {
         onMouseEnter={() => !isExpanded && setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
-        <BoxCubeIcon />
+        {/* Logo Link */}
+        <div className={styles.asideLogoWrapper}>
+          <Link href="/">
+            {isExpanded || isHovered ? (
+              <Image
+                src="/images/logo/text-logo-dark.svg"
+                alt="logo"
+                width={154}
+                height={40}
+              />
+            ) : (
+              <Image
+                src="/images/logo/logo-dark.svg"
+                alt="logo"
+                width={32}
+                height={32}
+              />
+            )}
+          </Link>
+        </div>
         {renderMenuItems(navItems, "main")}
         <h2 className={styles.logo}>Soliton Core</h2>
       </aside>
