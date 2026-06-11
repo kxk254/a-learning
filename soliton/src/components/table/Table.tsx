3330043182ff337ee813fe9623a700ce04ad57c4
konno <konno.kenji@gmail.com>
2026-06-11 14:04:00 +0900

    commit

--- soliton/src/components/table/Table.tsx
+++ soliton/src/components/table/Table.tsx
@@ -17,7 +17,7 @@ export type TableProps<T extends Record<string, unknown>> = {
   rowHeight?: string;
 };
 
-export default function Table<T extends Record<string, unkown>>({
+export default function Table<T extends Record<string, unknown>>({
   columns,
   data,
   onCellUpdate,
@@ -54,50 +54,53 @@ export default function Table<T extends Record<string, unkown>>({
       <tbody>
         {data.map((row, index) => (
           <tr key={index} style={{ height: rowHeight }}>
-            {columns.map((col) => (
-		    const isEdited = editedCells[getCellKey(index, col.key)];
-              <td
-                key={String(col.key)}
-                className={`${col.cellClassName ?? ""} ${isEdited ? styles.idEdited : ""}`}
-                style={{
-                  width: col.width,
-                  height: rowHeight,
-                }}
-                onClick={() => {
-                  setEditingCell({ row: index, key: String(col.key) });
-                  setEditValue(String(row[col.key] ?? ""));
-                }}
-              >
-                {editingCell?.row === index &&
-                editingCell?.key === String(col.key) ? (
-                  <input
-                    autoFocus
-                    value={editValue}
-                    style={{
-                      flex: 1,
-                      width: "100%",
-                      height: "100%",
-                      border: "none",
-                    }}
-                    onChange={(e) => setEditValue(e.target.value)}
-                    onBlur={() => {
-                      const original = String(data[index][col.key] ?? "");
-                      const updated = editValue;
-                      if (original !== updated) {
-                        setEditedCells((prev) => ({
-                          ...prev,
-                          [getCellKey(index, col.key)]: true,
-                        }));
-                        onCellUpdate?.(index, col.key, editValue);
-                      }
-                      setEditingCell(null);
-                    }}
-                  />
-                ) : (
-                  String(row[col.key] ?? "")
-                )}
-              </td>
-            ))}
+            {columns.map((col) => {
+              const isEdited = editedCells[getCellKey(index, col.key)] ?? false;
+              console.log("isEdited", isEdited);
+              return (
+                <td
+                  key={String(col.key)}
+                  className={`${col.cellClassName ?? ""} ${isEdited ? styles.idEdited : ""}`}
+                  style={{
+                    width: col.width,
+                    height: rowHeight,
+                  }}
+                  onClick={() => {
+                    setEditingCell({ row: index, key: col.key });
+                    setEditValue(String(row[col.key] ?? ""));
+                  }}
+                >
+                  {editingCell?.row === index &&
+                  editingCell?.key === col.key ? (
+                    <input
+                      autoFocus
+                      value={editValue}
+                      style={{
+                        flex: 1,
+                        width: "100%",
+                        height: "100%",
+                        border: "none",
+                      }}
+                      onChange={(e) => setEditValue(e.target.value)}
+                      onBlur={() => {
+                        const original = String(data[index][col.key] ?? "");
+                        const updated = editValue;
+                        if (original !== updated) {
+                          setEditedCells((prev) => ({
+                            ...prev,
+                            [getCellKey(index, col.key)]: true,
+                          }));
+                          onCellUpdate?.(index, col.key, editValue);
+                        }
+                        setEditingCell(null);
+                      }}
+                    />
+                  ) : (
+                    String(row[col.key] ?? "")
+                  )}
+                </td>
+              );
+            })}
           </tr>
         ))}
       </tbody>
