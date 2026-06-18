84bf18712c52fbf198b0ba1a1b617e9274b74548
konno <konno.kenji@gmail.com>
2026-06-18 23:14:55 +0900

    form

--- soliton/src/app/(app)/dashboard/page.tsx
+++ soliton/src/app/(app)/dashboard/page.tsx
@@ -1,14 +1,33 @@
 export default function dashboard() {
-  function printCoord(pt: { x: number; y: number }) {
-    return pt.x;
+  interface Options {
+    width: number;
   }
 
-  let a = printCoord({ x: 3, y: 7 });
+  function configure(x: Options | "auto") {
+    console.log("configure  ", x);
+    return x;
+  }
+
+  let a = configure({ width: 100 });
+  configure("auto");
+  configure("automatic");
+
+  a = String(a.width);
+
+  function map<Input, Output>(
+    arr: Input[],
+    func: (arg: Input) => Output,
+  ): Output[] {
+    return arr.map(func);
+  }
+
+  const parsed = map(["1", "2", "3", 4, 5], (n) => parseInt(n));
 
   return (
     <>
       {" "}
       <div>{a}</div>
+      <div>{parsed}</div>
     </>
   );
 }
