8328dfcbd96988f270a96ef6d52eb6389f19bb1c
konno <konno.kenji@gmail.com>
2026-06-20 11:06:47 +0900

    form

--- soliton/src/app/(app)/dashboard/page.tsx
+++ soliton/src/app/(app)/dashboard/page.tsx
@@ -1,10 +1,17 @@
 export default function dashboard() {
+  function double(n: number): number {
+    console.log(n * 2);
+    return n * 2;
+  }
+
+  const x = double(5);
+
   let a = "a";
   let parsed = "parsed";
   return (
     <>
       {" "}
-      <div>{a}</div>
+      <div>{x}</div>
       <div>{parsed}</div>
     </>
   );
