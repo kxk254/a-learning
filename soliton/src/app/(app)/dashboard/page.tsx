3fd3e16bd3b8bc66527feef3d950587e38a3d0e1
konno <konno.kenji@gmail.com>
2026-06-20 23:01:37 +0900

    form

--- soliton/src/app/(app)/dashboard/page.tsx
+++ soliton/src/app/(app)/dashboard/page.tsx
@@ -1,18 +1,12 @@
 export default function dashboard() {
-  function double(n: number): number {
-    console.log(n * 2);
-    return n * 2;
+  function minimumLength<Type extends { length: number }>(
+    obj: Type,
+    minimum: number,
+  ): number {
+    if (obj.length >= minimum) {
+      return obj.length;
+    } else {
+      return 2;
+    }
   }
-
-  const x = double(5);
-
-  let a = "a";
-  let parsed = "parsed";
-  return (
-    <>
-      {" "}
-      <div>{x}</div>
-      <div>{parsed}</div>
-    </>
-  );
 }
