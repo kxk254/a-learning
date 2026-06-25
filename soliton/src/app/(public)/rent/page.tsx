600d1d578b1f52a3de0aaa20e1af99411930904b
konno <konno.kenji@gmail.com>
2026-06-25 08:13:24 +0000

    commit

--- soliton/src/app/(public)/rent/page.tsx
+++ soliton/src/app/(public)/rent/page.tsx
@@ -2,23 +2,3 @@
 import TableRevenue, { Column } from "@/src/components/table/TableRevenue";
 import { Badge } from "@/src/components/ui/button/Badge";
 
-type Person = { name: string; age: number; city: string };
-
-export default function Page() {
-  const columns: Column<Person>[] = [
-    {
-      key: "name",
-      label: "Name",
-      render: (a) => <Badge color={"primary"}>{a.name}</Badge>,
-    },
-    { key: "age", label: "Age" },
-    { key: "city", label: "City" },
-  ];
-
-  const data: Person[] = [
-    { name: "Alice", age: 20, city: "New York" },
-    { name: "Bob", age: 33, city: "Denver" },
-  ];
-
-  return <TableRevenue columns={columns} data={data} />;
-}
--- soliton/src/components/table/TableNested.tsx
+++ soliton/src/components/table/TableNested.tsx
@@ -3,37 +3,11 @@ import React from "react";
 
 export type Column<T> = {
   key: keyof T;
-  label: string;
+  name: string;
   render?: (row: T) => React.ReactNode;
 };
 
 export type TableProps<T> = {
-  columns: Column<T>;
+  column: Column<T>;
   data: T[];
 };
-
-export default function TableNested({ columns, data }: TableProps<T>) {
-  return (
-    <table>
-      <thead>
-        <tr>
-          {columns.map((col) => (
-            <th key={String(col.key)}>{col.label}</th>
-          ))}
-        </tr>
-      </thead>
-
-      <tbody>
-        {data.map((r, rIndex) => (
-          <tr key={rIndex}>
-            {columns.map((col) => (
-              <td key={String(col.key)}>
-                {col.render ? col.render(r) : String(r[col.key])}
-              </td>
-            ))}
-          </tr>
-        ))}
-      </tbody>
-    </table>
-  );
-}
