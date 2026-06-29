07c6f60953c07a1cbfc7b987bc189369bc0793c2
konno <konno.kenji@gmail.com>
2026-06-29 22:48:27 +0900

    commit

--- soliton/pgtest/setup.sql
+++ soliton/pgtest/setup.sql
@@ -1,13 +1,14 @@
+
+CREATE TABLE cities (
+    name            varchar(80) ,
+    location        point
+);
+
 CREATE TABLE weather (
-    city            varchar(80) references cities(name),
+    city            varchar(80) ,
 
     temp_lo         int,           -- 最低気温
     temp_hi         int,           -- 最高気温
     prcp            real,          -- 降水量
     date            date
-);
-
-CREATE TABLE cities (
-    name            varchar(80) primary key,
-    location        point
 );
