d9504829291d952649c562164c174303c967ff64
konno <konno.kenji@gmail.com>
2026-07-04 00:01:57 +0900

    commit

--- soliton/src/app/(app)/admin/page.tsx
+++ soliton/src/app/(app)/admin/page.tsx
@@ -2,7 +2,8 @@
 import TableNested, { Column } from "@/src/components/table/TableNested";
 import { Badge } from "@/src/components/ui/button/Badge";
 import { StatusDot } from "@/src/components/ui/button/StatusDot";
-import { users, Order, User } from "@/src/data/test-data";
+import { users as initialUsers, Order, User } from "@/src/data/test-data";
+import { useState } from "react";
 
 export default function AdminAdmin() {
   const orderColumn: Column<Order>[] = [
@@ -16,12 +17,48 @@ export default function AdminAdmin() {
     { key: "name", label: "Name" },
     { key: "email", label: "Email" },
   ];
+  const [users, setUsers] = useState(initialUsers);
+  const usersUpdateCell = (row: React.Key, key: keyof User, value: string) => {
+    setUsers((prev) =>
+      prev.map((item, index) =>
+        index === row ? { ...item, [key]: value } : item,
+      ),
+    );
+  };
+  const ordersUpdateCell = (
+    usersRow: React.Key,
+    ordersRow: number,
+    key: keyof Order,
+    value: string,
+  ) => {
+    setUsers((users) =>
+      users.map((user) =>
+        user.id === Number(usersRow)
+          ? {
+              ...user,
+              orders: user.orders.map((order, i) =>
+                i === ordersRow ? { ...order, [key]: value } : order,
+              ),
+            }
+          : user,
+      ),
+    );
+  };
 
   return (
     <TableNested
       columns={userColumn}
       data={users}
-      renderRow={(p) => <TableNested columns={orderColumn} data={p.orders} />}
+      renderRow={(p) => (
+        <TableNested
+          columns={orderColumn}
+          data={p.orders}
+          onCellUpdate={(row, key, value) =>
+            ordersUpdateCell(p.id, Number(row), key, value)
+          }
+        />
+      )}
+      onCellUpdate={usersUpdateCell}
     />
   );
 }
