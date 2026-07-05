352729147c2e7f3c94a8cc24a2b58f8841698826
konno <konno.kenji@gmail.com>
2026-07-05 22:29:08 +0900

    commit

--- drf/solitonRE/views.py
+++ drf/solitonRE/views.py
@@ -1,3 +1,67 @@
 from django.shortcuts import render
+from .models import *
+from rest_framework import permissions, viewsets
+from .serializers import *
+from django.utils import timezone
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 
-# Create your views here.
+
+# Create your views here
+class TenantViewSet(viewsets.ModelViewSet):
+    queryset = Tenant.objects.all()
+    serializer_class = TenantSerializer
+
+
+class InvoiceIdViewSet(viewsets.ModelViewSet):
+    queryset = InvoiceId.objects.all().order_by("-id")
+    serializer_class = InvoiceIdSerializer
+
+
+class InvoiceTaxBreakdownViewSet(viewsets.ModelViewSet):
+    queryset = InvoiceTaxBreakdown.objects.all().order_by("-invoice_id")
+    serializer_class = InvoiceTaxBreakdownSerializer
+
+
+class RevenueViewSet(viewsets.ModelViewSet):
+    """
+    invoiceid is one-to-many
+    RevenueViewSet has many and InvoiceId has one [RevenueViewSet]=InvoiceId
+    InvoiceId = InvoiceTaxBreakdown linked via invoice
+    """
+
+    queryset = Revenue.objects.all()
+    serializer_class = RevenueSerializer
+
+    @action(detail=False, methods=["get"])
+    def by_invoices(self, request):
+        revenues = Revenue.objects.all()
+        invoices = InvoiceId.objects.all()
+        bukken = request.query_params.get("bukken")
+        month_now = request.query_params.get(
+            "months", timezone.now().strftime("%Y-%m-%d")
+        )
+        current_date = datetime.strptime(month_now, "%Y-%m-%d")
+        prev_year_date = current_date - relativedelta(months=2)
+        month_prev = prev_year_date.strftime("%Y-%m-%d")
+
+        invoice_ids = request.query_params.getlist("invoiceid")
+
+        if invoice_ids:
+            revenues = revenues.filter(invoiceid__in=invoice_ids)
+            invoices = invoices.filter(invoiceid__in=invoice_ids)
+        if bukken:
+            revenues = revenues.filter(bukken=bukken)
+            invoices = invoices.filter(bukken=bukken)
+        if month_now:
+            revenues = revenues.filter(report_date__range=[month_prev, month_now])
+            invoices = invoices.filter(report_date__range=[month_prev, month_now])
+
+        return Response(
+            {
+                "invoices": InvoiceIdSerializer(invoices, many=True).data,
+                "revenues": RevenueSerializer(revenues, many=True).data,
+            }
+        )
---
+++ soliton/src/data/soliton-type.tsx
@@ -0,0 +1,43 @@
+export interface Invoice {
+  id: number;
+  tenant_name: string;
+  bukken_name: string;
+  invoiceid: string;
+  room_no: string;
+  report_date: string;
+  revenue_bt_ttl_0: number;
+  revenue_bt_ttl: number;
+  tax_ttl: number;
+  revenue_at_ttl: number;
+  revenue_bt_gttl: number;
+  revenue_tax_gttl: number;
+  revenue_at_gttl: number;
+  pdf_issued_date: string | null;
+  mail_sent_date: string | null;
+  revenue: number;
+  tenant: number;
+  bukken: number;
+}
+
+export interface Revenue {
+  id: number;
+  tenant_name: string;
+  bukken_name: string;
+  account_code_name: string;
+  report_date: string;
+  start_date: string;
+  end_date: string;
+  invoice_date: string;
+  receipt_date: string;
+  revenue_bt: number;
+  revenue_tax: number;
+  revenue_at: number;
+  invoiceid: string;
+  invoice_processed: boolean;
+  invoice_head_note: string;
+  note: string | null;
+  tenant: number;
+  bukken: number;
+  contract: number;
+  account_code: number;
+}
